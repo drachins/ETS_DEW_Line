b821119b239bc02734d97a81661051b82d190200
drachins <djfrachinksky@gmail.com>
2024-05-05 13:01:24 -0600

    corrected syntax in data_download destructor

--- src/data_download.cpp
+++ src/data_download.cpp
@@ -78,14 +78,14 @@ size_t DataDownload::write_data_2(void* buffer, size_t size, size_t nmemb, void*
 
 DataDownload::~DataDownload(){
 
-    if(output1 ==  NULL){
+    if(output1 !=  NULL){
         delete output1;
     }
 
-    if(output2 == NULL){
+    if(output2 != NULL){
         delete output2;
     }
-    
+
     curl_easy_cleanup(curl_1);
     curl_easy_cleanup(curl_2);
 
