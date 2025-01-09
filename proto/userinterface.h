271ecfcff6299c2d7cf94951150571f216036e3e
drachins <54466790+drachins@users.noreply.github.com>
2025-01-09 12:40:35 -0700

    Update userinterface.h

--- proto/userinterface.h
+++ proto/userinterface.h
@@ -1,7 +1,21 @@
-#ifinded USERINTERFACE_H
+#ifindef USERINTERFACE_H
 #define USERINTERFACE_H
 
+#include<iostream>
 
+
+#include "realtimereader.h"
+
+class UserInterface{
+  public:
+    UserInterface();
+    void UserInput();
+    void SetRealTimeHandle(RealTimeReader* realtime){}
+    ~UserInterface();
+  private:
+
+    RealTimeReader* realtime;
+};
 
 
 #endif
